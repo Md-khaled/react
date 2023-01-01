2c720dcea9d610449913321fdfcb62646cb5ff0a
Md-khaled <ujjalbhuiyan123@gmail.com>
2023-01-01 13:27:47 +0600

    Revert "react hook"
    This reverts commit c158b0fd8919d244739b9d84d93617829e920843.

--- src/component/hooks/custom/useDocumentTitle.js
+++
@@ -1,9 +0,0 @@
-import {useEffect} from 'react'
-
-function useDocumentTitle(count) {
-    useEffect(() => {
-        document.title = `Countt - ${count}`
-    }, [count])
-}
-
-export default useDocumentTitle
